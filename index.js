82367543119984d1528db1035af11e0dfc3bbf65
Abhilash Kumar <142437925+Abhilash23kumar@users.noreply.github.com>
2023-10-07 12:26:27 +0530

    Add files via upload
    hi

---
+++ index.js
@@ -0,0 +1,79 @@
+let body1 = document.getElementById("prifile-menu");
+let hbody = document.getElementById("h-home");
+let ahbody = document.getElementsByClassName("ah");
+let h_button = document.getElementById("s-h-button");
+let abody = document.getElementById("about");
+let a_button = document.getElementById("s-a-button");
+let sbody = document.getElementById("skills");
+let s_button = document.getElementById("s-s-button");
+let ebody = document.getElementById("education");
+let e_button = document.getElementById("s-e-button");
+let cbody = document.getElementById("contact");
+let c_button = document.getElementById("s-c-button");
+let ibody = document.getElementById("images");
+let i_button = document.getElementById("s-i-button");
+// h_button.onclick = function() {
+//     if(body1.className == "black" ||hbody.className == "black"  ) {
+//         body1.className = "white";
+//         hbody.className = "white";
+//          abody.className = "black";
+//         sbody.className = "black";
+//         ebody.className = "black";
+//         cbody.className = "black";
+//         ibody.className = "black";
+//     }
+// };
+// a_button.onclick = function() {
+//     if(body1.className == "black" ||abody.className == "black"  ) {
+//         body1.className = "ab";
+//         abody.className = "ab";
+//         hbody.className = "black";
+//         sbody.className = "black";
+//         ebody.className = "black";
+//         cbody.className = "black";
+//         ibody.className = "black";
+//     }
+// };
+// s_button.onclick = function() {
+//     if(body1.className == "black" ||sbody.className == "black"  ) {
+//         body1.className = "sb";
+//         sbody.className = "sb";
+//         abody.className = "black";
+//         hbody.className = "black";
+//         ebody.className = "black";
+//         cbody.className = "black";
+//         ibody.className = "black";
+//     }
+// };
+// e_button.onclick = function() {
+//     if(body1.className == "black" ||ebody.className == "black"  ) {
+//         body1.className = "eb";
+//         ebody.className = "eb";
+//         hbody.className = "black";
+//         abody.className = "black";
+//         sbody.className = "black";
+//         cbody.className = "black";
+//         ibody.className = "black";
+//     }
+// };
+// c_button.onclick = function() {
+//     if(body1.className == "black" ||cbody.className == "black"  ) {
+//         body1.className = "cb";
+//         cbody.className = "cb";
+//         hbody.className = "black";
+//         abody.className = "black";
+//         sbody.className = "black";
+//         ebody.className = "black";
+//         ibody.className = "black";
+//     }
+// };
+// i_button.onclick = function(){
+//     if(body1.className == "black" ||ibody.className == "black"  ) {
+//         body1.className = "ib";
+//         ibody.className = "ib";
+//         hbody.className = "black";
+//         abody.className = "black";
+//         sbody.className = "black";
+//         ebody.className = "black";
+//         cbody.className = "black";
+//     }};
